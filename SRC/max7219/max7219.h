2ed01943122dca2b2c906a49d3f81442f0e6f7ad
Mitko Mitev <191958@vutbr.cz>
2023-05-27 22:47:43 +0200

    funcion declaration

---
+++ SRC/max7219/max7219.h
@@ -0,0 +1,18 @@
+#ifndef _MAX7219_H_
+#define _MAX7219_H_
+#include <Arduino.h>
+#include <SPI.h>
+
+  class max7219Matrix{
+    private:
+      void send_cmd(byte adres, byte data);
+    public:
+      void init_max7219();
+      void setup_bright(byte bright);
+      void setup_decode_mode(byte mode);
+      void setup_scan_limit(byte scanLimit);
+      void shut_down(byte data);
+      void setup_display_test(byte data);
+  };
+
+#endif
